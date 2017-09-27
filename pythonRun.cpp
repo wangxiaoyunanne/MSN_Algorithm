97d187dd0cc58bc25f3d0da9184d3ca33206302d
wangxiaoyunanne <wangxiaoyunanne@users.noreply.github.com>
2017-09-27 00:37:25 -0700

    Delete pythonRun.cpp

--- pythonRun.cpp
+++
@@ -1,7 +0,0 @@
-#include <python2.7/Python.h>
-int main(){
-    Py_SetProgramName("myPythonProgram");
-    Py_Initialize();
-    PyRun_SimpleString("exec(open('msnAlgRunner.py').read())");
-    Py_Finalize();
-}
